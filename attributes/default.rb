75a89ee9cdb7fedbfd6fbc2eed38138913555011
Sanjay Lokhande <sanjay.lokhande@opexsoftware.com>
2016-10-28 10:34:10 +0530

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,17 +1,17 @@
 # Attributes specific to the self healing server
-default['shs-server']['pull_req_time'] = "5"
-default['shs-server']['healingscript_repo_name'] = 'Self-Healing-Script'
-default['shs-server']['healingscript_repo_url'] = 'github.wdf.sap.corp/GLDS-SM/Self-Healing-Script.git'
-default['shs-server']['hrepo_username'] = ''
-default['shs-server']['hrepo_password'] = ''
-default['shs-server']['slim_php_autoloader'] = '/var/www/html/SHS/web/vendor/autoload.php'
-default['shs-server']['project_dir'] = '/var/www/html/SHS/web'
-default['shs-server']['rules_file_name'] = 'healingrules_v2.nb'
-default['shs-server']['apache_doc_root'] = '/var/www/html'
-default['shs-server']['escaped_apache_doc_root'] = '\\/var\\/www\\/html'
-default['shs-server']['webserver_service_name'] = 'httpd'
-default['shs-server']['apache_user'] = 'apache'
-default['shs-server']['cron_pkg_name'] = 'cronie'
+default['SHServer']['pull_req_time'] = "5"
+default['SHServer']['healingscript_repo_name'] = 'Self-Healing-Script'
+default['SHServer']['healingscript_repo_url'] = 'github.wdf.sap.corp/GLDS-SM/Self-Healing-Script.git'
+default['SHServer']['hrepo_username'] = ''
+default['SHServer']['hrepo_password'] = ''
+default['SHServer']['slim_php_autoloader'] = '/var/www/html/SHS/web/vendor/autoload.php'
+default['SHServer']['project_dir'] = '/var/www/html/SHS/web'
+default['SHServer']['rules_file_name'] = 'healingrules_v2.nb'
+default['SHServer']['apache_doc_root'] = '/var/www/html'
+default['SHServer']['escaped_apache_doc_root'] = '\\/var\\/www\\/html'
+default['SHServer']['webserver_service_name'] = 'httpd'
+default['SHServer']['apache_user'] = 'apache'
+default['SHServer']['cron_pkg_name'] = 'cronie'
 
 # Attributes specific to log rotation on server/client
 default['logrotate']['filesize'] = '1M'
@@ -19,7 +19,6 @@ default['logrotate']['num_of_file_rotation'] = '4'
 default['logFiles']['remove_before_no_of_days'] = '7'
 
 # Attributes specific to serf installation on server/client
-#default['serf']['serf_url'] = 'http://moo-repo.wdf.sap.corp:8080/static/lit/GLDS/postinstall/linux/sources/serf/amd64/0.7.0/serf'
 default['serf']['serf_url'] = 'https://releases.hashicorp.com/serf/0.7.0/serf_0.7.0_linux_amd64.zip'
 default['serf']['encrypt_key'] = 'OKo8QmHU5SoLcdVGWARt5A=='
 
